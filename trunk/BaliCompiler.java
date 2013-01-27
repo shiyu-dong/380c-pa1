8cd8aec8acb66d0d411ca7bb4d7d58f3700db42c
francis.zw@gmail.com <francis.zw@gmail.com@b1ec519a-7e14-ef54-2d96-59258dafa544>
2013-01-27 00:36:48 +0000

    coding complete, first draft

--- trunk/BaliCompiler.java
+++ trunk/BaliCompiler.java
@@ -6,7 +6,11 @@ import edu.cornell.cs.sam.io.Tokenizer.TokenType;
 
 public class BaliCompiler
 {
+  static Hashtable<String, Integer> method_list;
+  static int label_count;
+
   public static void main(String[] args) {
+    label_count = 0;
     if (args.length < 2) {
       // check input arguments
       System.out.println("Please specify input and output file names");
@@ -86,8 +90,9 @@ public class BaliCompiler
     //in the tokenizer.
     //TODO: add appropriate exception handlers to generate useful error msgs.
     String methodName= "";
-    String formals = "";
     String pgm = "";
+    int offset;
+
     Hashtable<String, Integer>  symt = new Hashtable<String, Integer>();
     symt.put("rv", symt.size());
 
@@ -100,14 +105,15 @@ public class BaliCompiler
       // method name
       methodName = f.getWord();
       pgm += methodName + ":\n";
-      System.out.println("Method Name: "+methodName);
 
       // open parenthesis
       if (!f.check ('(')) {
         throw new Exception("Expect '(' in method decleartion");
       }
       // formals
-      formals = getFormals(f, symt);
+      int input_num = getFormals(f, symt);
+      method_list.put(methodName, input_num);
+      //System.out.println("Method Name: "+methodName);
       // open parenthesis
       if (!f.check (')')) {
         throw new Exception("Expect ')' in method decleartion");
@@ -121,7 +127,12 @@ public class BaliCompiler
 
       pgm += getDeclarations(f, symt);
 
-      pgm += getStatements(f, symt);
+      pgm += getStatements(f, symt, methodName, -1);
+
+      offset = symt.get("rv") - symt.get("FBR");
+      pgm += methodName + "End:\n"
+              + "STOREOFF " + offset + "\n"
+              + "JUMPIND";
 
       if (!f.check ('}')) {
         throw new Exception("Expect '}' in method decleartion");
@@ -143,19 +154,21 @@ public class BaliCompiler
     return null;
   }
 
-  static String getFormals(SamTokenizer f, Hashtable<String, Integer> symt) throws Exception {
+  static int  getFormals(SamTokenizer f, Hashtable<String, Integer> symt) throws Exception {
     String ID;
     String code="";
+    int count = 0;
     while(!f.test(')')) {
       if (!f.check("int")) {
         throw new Exception("Expect 'int' in formal");
       }
       ID = f.getWord();
       symt.put(ID, symt.size());
-      System.out.print("ID: "+ ID+"\n");
+      count ++;
+      //System.out.print("ID: "+ ID+"\n");
       if (f.test(')')) {
         System.out.print("Done with Formal");
-        return ID; // need to fix..
+        return count;
       }
       if (!f.check(',')) {
         throw new Exception("Expect 'int' in formal");
@@ -163,7 +176,7 @@ public class BaliCompiler
     }
     System.out.print("Done with Formal");
 
-    return null;
+    return 0;
   }
 
   static String getDeclarations(SamTokenizer f, Hashtable<String, Integer> symt) throws Exception {
@@ -180,12 +193,14 @@ public class BaliCompiler
     return pgm;
   }
 
-  static String getDeclaration(SamTokenizer f, Hashtable<String, Integer> symt) {
+  static String getDeclaration(SamTokenizer f, Hashtable<String, Integer> symt) throws Exception{
     String pgm = "";
     String ID;
+    Character c;
 
-    System.out.println("!!");
-    ID = f.getString();
+    System.out.println(f.peekAtKind());
+    ID = f.getWord();
+    System.out.println(ID);
     symt.put(ID, symt.size());
     if (f.test('=')) {
       f.check('=');
@@ -201,21 +216,200 @@ public class BaliCompiler
     return pgm;
   }
 
+  static String getStatements(SamTokenizer f, Hashtable<String, Integer> symt, String methodName, int current_while_label) throws Exception{
+    String pgm = "";
+    String tmp;
 
-  static String getStatements(SamTokenizer f, Hashtable<String, Integer> symt) {
-    return null;
+    while (!f.test('}')) {
+      // empty statement ';'
+      if (f.test(';')) {
+        f.check(';');
+        continue;
+      }
+
+      // block
+      if (f.test('{')) {
+        f.check('{');
+        pgm += getStatements(f, symt, methodName, current_while_label);
+        if (!f.check('}')) {
+          throw new Exception("Expect '}' at the end of the statement block");
+        }
+      }
+
+      tmp = f.getWord();
+
+      // return
+      if (tmp.equals("return")) {
+        pgm += getExp(f, symt);
+        return pgm + "JUMP " + methodName + "End\n";
+      }
+
+      // if e then B1 else B2
+      if (tmp.equals("if")) {
+        int current_label_count = label_count;
+        label_count++;
+
+        if (!f.check('('))
+          throw new Exception("Expect '(' after 'if'");
+        pgm += getExp(f, symt); // code for if condition e
+        if (!f.check(')'))
+          throw new Exception("Expect ')' after 'if'");
+        pgm += "JUMPC " +"Taken" + current_label_count + "\n"; // branch taken
+
+        String B1 = getStatements(f, symt, methodName, current_while_label);
+
+        if (!f.check("else"))
+          throw new Exception("Expect 'else after 'if'");
+
+        String B2 = getStatements(f, symt, methodName, current_while_label);
+
+        pgm += B2;
+        pgm += "JUMP BranchEnd" + current_label_count + "\n";
+        pgm += "Taken" + current_label_count + ":\n";
+        pgm += B1;
+        pgm += "BranchEnd" + current_label_count + ":\n";
+      }
+
+      // while ( E ) B
+      if (tmp.equals("while")) {
+        int current_label_count = label_count;
+        label_count++;
+
+        pgm += "JUMP " + "WhileLabel1_" + current_label_count + "\n";
+        pgm += "WhileLabel2_" + current_label_count + ":\n";
+
+        if (!f.check('('))
+          throw new Exception("Expect '(' after 'while'");
+        String E = getExp(f, symt); // code for while condition E
+        if (!f.check(')'))
+          throw new Exception("Expect ')' after 'while'");
+
+        String B = getStatements(f, symt, methodName, current_label_count);
+
+        pgm += B;
+        pgm += "WhileLabel1_" + current_label_count + ":\n";
+        pgm += E;
+        pgm += "JUMPC WhileLabel2_"+current_label_count + "\n";
+        pgm += "WhileLabel3_" + current_label_count + ":\n";
+      }
+
+      // break
+      if (tmp.equals("break")) {
+        if (!f.check(";")) {
+          throw new Exception("Expecting ';' after break");
+        }
+        if (current_while_label == -1) {
+          throw new Exception("'break' not used inside a loop");
+        }
+        pgm += "JUMP WhileLabel3_" + current_while_label + "\n";
+      }
+
+
+    }
+    return pgm;
   }
 
-  static String getExp(SamTokenizer f, Hashtable<String, Integer> symt) {
-//    switch (f.peekAtKind()) {
-//      case INTEGER: //E -> integer
-//        return "PUSHIMM " + f.getInt() + "\n";
-//      case OPERATOR:  
-//        {
-//        }
-//      default:   return "ERROR\n";
-//    }
+  static String getExp(SamTokenizer f, Hashtable<String, Integer> symt) throws Exception{
+    switch (f.peekAtKind()) {
+      case INTEGER: // E -> LITERAL -> INT
+      {
+        return "PUSHIMM " + f.getInt() + "\n";
+      }
+
+      case WORD:
+      {
+        String tmp = f.getWord();
+        if (tmp.equals("true")) { // E -> LITERAL -> true
+          return "PUSHIMM 1\n";
+        }
+        if (tmp.equals("false")) { // E -> LITERAL -> false
+          return "PUSHIMM 0\n";
+        }
+        if (method_list.contains(tmp)) { // E -> METHOD ( ACTUALS )
+          String pgm = "PUSHIMM 0\n";
+          if (!f.check('(')) {
+            throw new Exception("expecting '(' in functions actuals");
+          }
+          while (!f.test(')')) {
+            pgm += getExp(f, symt);
+          }
+          if (!f.check(')')) {
+            throw new Exception("expecting ')' in functions actuals");
+          }
+          return (pgm +
+                  "LINK\n" +
+                  "JSR " + tmp + "\n" +
+                  "POPFBR\n" +
+                  "ADDSP " + method_list.get(tmp) + "\n");
+        }
+        if (symt.contains(tmp)) { // E -> LOCATION -> ID
+          int offset = symt.get(tmp) - symt.get("FBR");
+          return "PUSHIMM " + offset;
+        }
+        break;
+      }
+
+      case OPERATOR:
+      {
+        String pgm = "";
+        char op;
+        if (!f.check('('))
+          throw new Exception("Expecting '('");
+        if (f.test('-')) {
+          f.check('-');
+          pgm += getExp(f, symt);
+          pgm += "PUSHIMM 0\n" + "SUB\n";
+          if (!f.check(')'))
+            throw new Exception("Expecting ')'");
+          return pgm;
+        }
+        if (f.test('!')) {
+          f.check('!');
+          pgm += getExp(f, symt);
+          pgm += "NOT\n";
+          if (!f.check(')'))
+            throw new Exception("Expecting ')'");
+          return pgm;
+        }
+
+        // E -> ( E ...
+        pgm += getExp(f, symt);
+
+        // E -> ( E )
+        if (f.test(')')) {
+          return pgm;
+        }
+
+        // E -> ( E ? E )
+        op = f.getOp();
+        pgm += getExp(f, symt);
+
+        switch (op) {
+          case '+':
+            return pgm + "ADD\n";
+          case '-':
+            return pgm + "SUB\n";
+          case '*':
+            return pgm + "TIMES\n";
+          case '/':
+            return pgm + "DIV\n";
+          case '&':
+            return pgm + "AND\n";
+          case '|':
+            return pgm + "OR\n";
+          case '<':
+            return pgm + "LESS\n";
+          case '>':
+            return pgm + "GREATER\n";
+          case '=':
+            return pgm + "EQUAL\n";
+        }
+        break;
+      }
+
+      default:
+        throw new Exception("Cannot recognize the expression");
+    }
     return null;
   }
-
 }
