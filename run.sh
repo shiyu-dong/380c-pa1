f4e25315bbfdb8ee4ffe220b648fffa4d381aa05
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@b1ec519a-7e14-ef54-2d96-59258dafa544>
2013-01-30 20:02:39 +0000

    edit run.sh to run jar file instead of java file

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-ls examples/good.*.bali | awk '{print "java -cp .:SaM-2.6.2.jar BaliCompiler ./" $1 " " $1 ".sam; echo " $1 "; echo "}' | sh
-ls tests/*.bali | awk '{print "java -cp .:SaM-2.6.2.jar BaliCompiler ./" $1 " " $1 ".sam; echo " $1 "; echo "}' | sh
+#ls examples/good.*.bali | awk '{print "java -cp .:SaM-2.6.2.jar BaliCompiler ./" $1 " " $1 ".sam; echo " $1 "; echo "}' | sh
+#ls tests/*.bali | awk '{print "java -cp .:SaM-2.6.2.jar BaliCompiler ./" $1 " " $1 ".sam; echo " $1 "; echo "}' | sh
+ls examples/good.*.bali | awk '{print "java -jar BaliCompiler.jar ./" $1 " " $1 ".sam; echo " $1 "; echo "}' | sh
+ls tests/*.bali | awk '{print "java -jar BaliCompiler.jar ./" $1 " " $1 ".sam; echo " $1 "; echo "}' | sh
